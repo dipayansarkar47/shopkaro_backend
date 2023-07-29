3f9d70bdcd00d9e99731bc93583830652734f359
dipayansarkar47 <dipayansarkar265482@gmail.com>
2023-07-30 00:24:04 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const server = express();
+const mongoose = require('mongoose');
+const { createProduct } = require('./controller/Product');
+const productsRouter = require('./routes/Products');
+const categoriesRouter = require('./routes/Categories');
+const brandsRouter = require('./routes/Brands');
+const cors = require('cors')
+
+server.use(cors({
+    exposedHeaders:['X-Total-Count']
+}))
+server.use(express.json()); // to parse req.body
+server.use('/products', productsRouter.router);
+server.use('/categories', categoriesRouter.router)
+server.use('/brands', brandsRouter.router)
+
+main().catch(err=> console.log(err));
+async function main(){
+    await mongoose.connect('mongodb://127.0.0.1:27017/shopkaro');
+    console.log('connected to database');
+}
+
+server.get('/',(req,res)=>{
+    res.json({status:'success'})
+})
+
+
+server.listen(8000,()=>{
+    console.log('server running');
+})
